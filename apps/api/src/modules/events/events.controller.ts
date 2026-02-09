7e2519b4b6df07e403463310f5390fe78fa36b2e
Luciano Figueiredo <lucianofigueiredo.dev@gmail.com>
2026-02-09 20:52:39 -0300

    fix: applying pipeline in correct location

--- apps/api/src/modules/events/events.controller.ts
+++ apps/api/src/modules/events/events.controller.ts
@@ -6,7 +6,6 @@ import {
   Get,
   Patch,
   Delete,
-  UsePipes,
 } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { ZodValidationPipe } from 'nestjs-zod';
@@ -19,10 +18,10 @@ export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
   @Post()
-  @UsePipes(new ZodValidationPipe(CreateEventSchema))
   create(
     @Param('simulationId') simulationId: string,
-    @Body() createEventDto: CreateEventDto,
+    @Body(new ZodValidationPipe(CreateEventSchema))
+    createEventDto: CreateEventDto,
   ) {
     return this.eventsService.create(simulationId, createEventDto);
   }
@@ -33,8 +32,11 @@ export class EventsController {
   }
 
   @Patch(':id')
-  @UsePipes(new ZodValidationPipe(UpdateEventSchema))
-  update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
+  update(
+    @Param('id') id: string,
+    @Body(new ZodValidationPipe(UpdateEventSchema))
+    updateEventDto: UpdateEventDto,
+  ) {
     return this.eventsService.update(id, updateEventDto);
   }
 
